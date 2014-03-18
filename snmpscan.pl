ec8cec5600e1e8b8c0c8676dab0734cb842c38f1
Sean McAdam <sean@fredcom.com>
2014-03-18 08:19:52 -0400

    Added some comments

--- snmpscan.pl
+++ snmpscan.pl
@@ -8,6 +8,15 @@
 # Purpose:
 # Tool for scanning SNMP devices accross wide swaths of IP ranges
 #
+# Provide two files for input: list of SNMP strings, and a list of CIDRs to scan
+# The script will:
+#    import the list of SNMP strings
+#    import the list of CIDRs and generate the IPs to check
+#    ping each IP first, and skip if no response
+#    Start to scan the IP with V2 first going though the SNMP strings until one responds
+#    Gather System Name, Location, and contact info
+#    Loop until all IPs have been checked
+#    print out a CSV of the data
 #
 
 use Carp;
